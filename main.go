d8cfbba811463d57256b5a8e6bd9e0f1042a8405
badiwidya <adiwidya.berton13@gmail.com>
2025-07-15 11:57:00 +0700

    Create cache dir to save html template

--- main.go
+++ main.go
@@ -4,9 +4,32 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 func main() {
+	host := runtime.GOOS
+
+	homeEnv := func() string {
+		if host == "windows" {
+			return "%USERPROFILE%"
+		}
+		return "$HOME"
+	}()
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("%s is not set.\n", homeEnv)
+		os.Exit(1)
+	} else {
+		cachePath := filepath.Join(home, ".cache", "mdserve")
+		err := os.MkdirAll(cachePath, 0755)
+		if err != nil {
+			fmt.Printf("Error: failed to make cache directory.\n")
+			os.Exit(1)
+		}
+	}
+
 	if len(os.Args) != 2 {
 		fmt.Printf("USAGE: mdserve [markdown file]\n")
 		os.Exit(1)
